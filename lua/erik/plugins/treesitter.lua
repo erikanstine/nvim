423fe341ea35e7aeee93d21a8ed3bdef42a7623c
Erik Anstine <erikanstine@gmail.com>
2023-02-01 16:24:29 -0500

    Add existing good state

---
+++ lua/erik/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
+
+treesitter.setup({
+	highlight = {
+		enable = true,
+	},
+	indent = { enable = true },
+	autotag = { enable = true },
+	ensure_installed = {
+		"json",
+		"lua",
+		"dockerfile",
+		"gitignore",
+	},
+	auto_install = true,
+})
