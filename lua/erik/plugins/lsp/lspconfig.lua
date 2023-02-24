30f7157320c6a9f8c5e661200dc30f47fa3b5d2b
Erik Anstine <erik.anstine@tink.com>
2023-02-24 13:47:15 -0500

    sumneko_lua -> lua_ls

--- lua/erik/plugins/lsp/lspconfig.lua
+++ lua/erik/plugins/lsp/lspconfig.lua
@@ -60,7 +60,7 @@ lspconfig["html"].setup({
 	on_attach = on_attach,
 })
 
-lspconfig["sumneko_lua"].setup({
+lspconfig["lua_ls"].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 	settings = { -- custom settings for lua
